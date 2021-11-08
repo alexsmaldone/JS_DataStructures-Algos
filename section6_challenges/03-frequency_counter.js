cce4edcec0ce1fb48877ef9d6b499e2a85d444fb
alexsmaldone <alexsmaldone@gmail.com>
2021-11-08 14:03:28 -0500

    sec6 challenges

---
+++ section6_challenges/03-frequency_counter.js
@@ -0,0 +1,11 @@
+/*
+Write a function called sameFrequency. Given two positive integers,
+find out if the two numbers have the same frequency of digits.
+Solution must have time complexity of O(N).
+*/
+
+function sameFrequency() {}
+
+console.log(sameFrequency(182, 281)); //true
+console.log(sameFrequency(34, 14)); //false
+console.log(sameFrequency(3589578, 5879385)); //true
---
+++ section6_challenges/04-freqCounter-multiplePointer.js
@@ -0,0 +1,23 @@
+/*
+Implement a function called areThereDuplicates which accepts a variable number
+of arguments and checks whether there are any duplicates among the arguments
+passed in. You can solve this using the frequency pattern OR
+the multiple pointers pattern.
+*/
+
+function areThereDuplicates(...args) {
+  //frequency solution
+  let collection = {};
+  for (let val in args) {
+    if (collection[args[val]]) collection[args[val]]++;
+    if (!collection[args[val]]) collection[args[val]] = 1;
+  }
+
+  for (let key in collection) {
+    if (collection[key] > 1) return true;
+  }
+  return false;
+}
+
+console.log(areThereDuplicates(1, 2, 3)); //false
+console.log(areThereDuplicates(1, 2, 2)); //true
