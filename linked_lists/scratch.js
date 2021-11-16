58cf93cc401b1b7098e93086373595d00066b23d
alexsmaldone <alexsmaldone@gmail.com>
2021-11-15 23:31:00 -0500

    linked list pop method

--- linked_lists/scratch.js
+++ linked_lists/scratch.js
@@ -41,7 +41,23 @@ class SinglyLinkedList {
 - decrement the length of the list by 1
 - return value of node removed
 */
-  pop() {}
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();
