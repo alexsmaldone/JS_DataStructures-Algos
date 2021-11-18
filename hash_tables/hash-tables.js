edf56359c287da195b74308549849686bb3596b4
alexsmaldone <alexsmaldone@gmail.com>
2021-11-17 22:10:18 -0500

    hash table set and get

--- hash_tables/hash-tables.js
+++ hash_tables/hash-tables.js
@@ -12,6 +12,7 @@ function hash(key, arrayLen) {
 }
 */
 
+//sort of constant time, limited at 100 iterations or O(100n)
 function hash(key, arrayLen) {
   let total = 0;
   let WEIRD_PRIME = 31;
@@ -21,4 +22,53 @@ function hash(key, arrayLen) {
     total = (total * WEIRD_PRIME + value) % arrayLen;
   }
   return total;
+}
+
+//HASHTABLE CLASS
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  /*
+  Accepts a key and value
+  - hashes the key
+  - stores key-value pair in the hash table array via separate chaining
+  */
+  set(key, val) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+
+  /*
+  Accepts key
+  - hashes key
+  - retrieves the key-value pair in the hash table
+  - if key isn't found, returns undefined
+  */
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
