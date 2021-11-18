5a9065f9fb0c1ca8b6f383537b5f8f21a7d5a9c2
alexsmaldone <alexsmaldone@gmail.com>
2021-11-17 22:23:04 -0500

    hash table keys and values

--- hash_tables/hash-tables.js
+++ hash_tables/hash-tables.js
@@ -71,4 +71,34 @@ class HashTable {
     }
     return undefined;
   }
+
+  // Loops thru hash table array and returns an array of keys in the table
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+
+  // Loops thru hash table array and returns an array of values in the table
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][0])) {
+            valuesArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
 }
