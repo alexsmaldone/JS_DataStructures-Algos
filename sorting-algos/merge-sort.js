6e3101884ad5e104c2c8298b76021122bfab794b
alexsmaldone <alexsmaldone@gmail.com>
2021-11-20 18:19:39 -0500

    implemented mergeSort

--- sorting-algos/merge-sort.js
+++ sorting-algos/merge-sort.js
@@ -14,9 +14,6 @@ Merging Arrays PseudoCode
   - once we exhaust one array, push in all the reamining values from the other array
 */
 
-let arr1 = [1, 3, 5];
-let arr2 = [2, 4, 6];
-
 // MERGE FUNCTION
 function merge(arr1, arr2) {
   let result = [];
@@ -47,7 +44,22 @@ function merge(arr1, arr2) {
 
 /*
 MergeSort PseudoCode
-- break u p array into halves until you have arrays that are empty or have one element
+- break up array into halves until you have arrays that are empty or have one element
 - Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
 - once the array has been merged back together, return the merged (and sorted!) array;
 */
+
+// MergeSort Function
+
+let arr1 = [10, 24, 76, 73, 72, 1, 9];
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort(arr1));
