87aa1574785f67335ac2f2dbfd3077af711c5e78
alexsmaldone <alexsmaldone@gmail.com>
2021-11-20 18:12:35 -0500

    merge sort merge function

--- sorting-algos/insertion-sort.js
+++ sorting-algos/insertion-sort.js
@@ -13,13 +13,13 @@ function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
     let j = i - 1;
-    while ((j > -1) && (currentVal < arr[j])) {
-      arr[j+1] = arr[j];
-      j--
+    while (j > -1 && currentVal < arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-    arr[j+1] = currentVal;
-}
-return arr;
+    arr[j + 1] = currentVal;
+  }
+  return arr;
 }
 
 let arr1 = [4, 1, 6, 7, 2, 9, 3, 20, 12];
---
+++ sorting-algos/merge-sort.js
@@ -0,0 +1,53 @@
+/*
+Merging Arrays
+- in order to implement a merge sort, it's useful to first implement a function responsible for merging 2 sorted arrays
+- given 2 sorted arrays, this helper function should create a new array which is also sorted, and consists of all the elements in the two input arrays
+- THis function should run in O(n+m) time and O(n+m) space complexity
+*/
+
+/*
+Merging Arrays PseudoCode
+- create an empty array, take a look at the smallest values in each input array
+- while there are still values we haven't looked at
+  - if the value in the first array is smaller than the value in the second array, push value in the first array into our results and move on to the next value in the first array
+  - if the value in the first array is larger than the value in the second array, pus hvalue in the second array into our results and move on to the next value in the second array
+  - once we exhaust one array, push in all the reamining values from the other array
+*/
+
+let arr1 = [1, 3, 5];
+let arr2 = [2, 4, 6];
+
+// MERGE FUNCTION
+function merge(arr1, arr2) {
+  let result = [];
+
+  let arr1Indx = 0;
+  let arr2Indx = 0;
+
+  while (arr1Indx < arr1.length && arr2Indx < arr2.length) {
+    if (arr2[arr2Indx] > arr1[arr1Indx]) {
+      result.push(arr1[arr1Indx]);
+      arr1Indx++;
+    } else {
+      result.push(arr2[arr2Indx]);
+      arr2Indx++;
+    }
+  }
+  while (arr1Indx < arr1.length) {
+    result.push(arr1[arr1Indx]);
+    arr1Indx++;
+  }
+  while (arr2Indx < arr2.length) {
+    result.push(arr2[arr2Indx]);
+    arr2Indx++;
+  }
+
+  return result;
+}
+
+/*
+MergeSort PseudoCode
+- break u p array into halves until you have arrays that are empty or have one element
+- Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+- once the array has been merged back together, return the merged (and sorted!) array;
+*/
