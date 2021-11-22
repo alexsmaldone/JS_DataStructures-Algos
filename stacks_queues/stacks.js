3560ee71aacab385c6e5cc6adfccaa57e92da813
alexsmaldone <alexsmaldone@gmail.com>
2021-11-21 19:26:37 -0500

    stack implementation

---
+++ stacks_queues/stacks.js
@@ -0,0 +1,62 @@
+// ARRAY Implementation - using PUSH and POP for your stack structure
+/*
+let stack = [];
+
+stack.push("google");
+stack.push("instagram");
+stack.push("Youtube");
+
+stack.pop();
+
+*/
+/*
+- using Shift to remove and Unshift to add
+*/
+
+// Implementing stack using a SinglyLinkedList
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+let stack = new Stack();
+console.log(stack.push(1));
+console.log(stack.push(2));
+console.log(stack.push(3));
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
