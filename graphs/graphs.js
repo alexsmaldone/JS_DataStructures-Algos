f870036e023a990a7be1631c031873fb943734e2
alexsmaldone <alexsmaldone@gmail.com>
2021-11-23 22:21:37 -0500

    graphs vertex

---
+++ graphs/graphs.js
@@ -0,0 +1,17 @@
+//Undirected Graph
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+}
+
+let g = new Graph();
+
+g.addVertex("Tokyo");
+
+console.log(g.adjacencyList);
