b341ca9ac6a6c7d79c4c0929b57ef2b283982e8b
alexsmaldone <alexsmaldone@gmail.com>
2021-11-24 00:41:57 -0500

    breadth and depth first traversal

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -30,16 +30,113 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  /*
+  DFS recurisve
+  - if vertex is empty
+    - return (this is base case)
+  - add vertex to results list
+  - mark vertex as visited
+  - for each neighbor in vertex's neighbors:
+    - if neighbor is not visited:
+      - recursively call DFS on neighbor
+  */
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+    return result;
+  }
+
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  /*
+  BreadthFirst PseudoCode
+  - create queue and place the starting vertex in it
+  - create array to store nodes visited
+  - creat an object ot store nodes visited
+  - Mark the starting vertex as visited
+  - Loop as long as there is anything in the queue
+  - Remove first vertex from the queue and push it into the array that stores nodes visited
+  - Loop over each vertex in the adjacency list for the vertex you are visiting
+  - If it is not inside the oibject that stores nodes visited, mark it as visited and enqueue that vertex
+  - noce finished looping, return array of visited nodes
+  */
+  breadthFirstTraversal(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 let g = new Graph();
 
-g.addVertex("Tokyo");
-g.addVertex("Dallas");
-g.addVertex("Aspen");
-g.addEdge("Dallas", "Tokyo");
-g.addEdge("Dallas", "Aspen");
-g.removeEdge("Dallas", "Tokyo");
-g.removeVertex("Dallas");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
 
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
 console.log(g.adjacencyList);
+console.log(g.breadthFirstTraversal("A"));
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
